1c20336da4ef6cf78990e1ab230c0b71c8d2927f
jata2011 <jata2011@gmail.com>
2017-06-03 12:51:22 +1000

    LeanFT Project
    desc

---
+++ LeanFtTestProject2/LeanFtTestProject2/FunctionClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeanFtTestProject2
+{
+    class FunctionClass
+    {
+        ApplicationModel1 apm;
+
+        public void Search() {
+
+            
+            apm.GoogleSearchButton.Click();
+
+
+
+        }
+    }
+}
---
+++ LeanFtTestProject2/LeanFtTestProject2/LeanFtTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using NUnit.Framework;
+using HP.LFT.SDK;
+using HP.LFT.Verifications;
+using HP.LFT.SDK.Web;
+
+namespace LeanFtTestProject2
+{
+    [TestFixture]
+    public class LeanFtTest : UnitTestClassBase
+    {
+
+        IBrowser OBrowser;
+        ApplicationModel1 apm;
+
+
+        [OneTimeSetUp]
+        public void TestFixtureSetUp()
+        {
+            // Setup once per fixture
+
+            OBrowser = BrowserFactory.Launch(BrowserType.InternetExplorer);
+            apm = new ApplicationModel1(OBrowser);
+
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Before each test
+        }
+
+        [Test]
+        public void Test()
+        {
+            OBrowser.Navigate("Google.com");
+
+            OBrowser.Sync();
+            apm.SearchEditField.SetValue("abc");
+            apm.GoogleSearchButton.Click();
+
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // Clean up after each test
+            OBrowser.Close();
+        }
+
+        [OneTimeTearDown]
+        public void TestFixtureTearDown()
+        {
+            // Clean up once per fixture
+        }
+    }
+}
